5f45b0fd010b4f684b78e357ab1518c6b9a63dc2
spocot <spencerkocot@gmail.com>
2019-05-15 23:42:14 -0400

    Finished defining what CHIP8 struct should contain

--- src/emu/emu.rs
+++ src/emu/emu.rs
@@ -1,8 +1,16 @@
 
 pub struct Chip8 {
-    opcode: u16,
+    opcode: u16, // Current opcode
     memory: [u8; 4096],
     registers: [u8; 16],
-    index: u16,
-    pc: u16
+    index: u16, // Index register
+    pc: u16, // Program counter
+    gfx: [u8; 2048], // Pixel values (64 x 32 screen)
+
+    // When set > zero, these timer registers will count down to zero
+    // System buzzer should sound whenever either timer reaches zero
+    delay_timer: u8,
+    sound_timer: u8,
+
+    key: [u8; 16], // Current key state
 }
