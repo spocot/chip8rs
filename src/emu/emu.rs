15ce567a3dc42c8553244d2b48723a8d255ccb14
Spencer Kocot <spencerkocot@gmail.com>
2019-09-29 23:43:17 -0400

    Added init method and constructor sets pc and loads fonts into mem.

--- src/emu/emu.rs
+++ src/emu/emu.rs
@@ -1,13 +1,31 @@
 use std::fmt;
 
-#[derive(Debug)]
+const FONTSET: [u8; 80] = [
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+];
+
 pub struct Chip8 {
     opcode: u16, // Current opcode
-    //memory: [u8; 4096],
+    memory: [u8; 4096],
     registers: [u8; 16],
     index: u16, // Index register
     pc: u16, // Program counter
-    //gfx: [u8; 2048], // Pixel values (64 x 32 screen)
+    gfx: [u8; 2048], // Pixel values (64 x 32 screen)
 
     // When set > zero, these timer registers will count down to zero
     // System buzzer should sound whenever either timer reaches zero
@@ -22,17 +40,51 @@ pub struct Chip8 {
 
 impl Chip8 {
     pub fn new() -> Chip8 {
-        Chip8 {
-            opcode: 0,
+        let mut c = Chip8 {
+            opcode: 0x200, // pc starts here
+            memory: [0; 4096],
             registers: [0; 16],
             index: 0,
             pc: 0,
+            gfx: [0; 2048],
             delay_timer: 0,
             sound_timer: 0,
             stack: [0; 16],
             sp: 0,
             key: [0; 16]
+        };
+
+        c.fontset_into_mem();
+        c
+    }
+
+    fn fontset_into_mem(&mut self) {
+        // Load fontset into memory.
+        for i in 0..80 {
+            self.memory[i] = FONTSET[i];
         }
+    }
+
+    pub fn init(&mut self) {
+        // Chip8 program counter starts at 0x200
+        self.pc = 0x200;
+
+        // Reset opcode, index, and stack pointer.
+        self.opcode = 0;
+        self.index = 0;
+        self.sp = 0;
+
+        // Clear display, stack, registers, and memory.
+        self.gfx.iter_mut().for_each(|x| *x = 0);
+        self.stack.iter_mut().for_each(|x| *x = 0);
+        self.registers.iter_mut().for_each(|x| *x = 0);
+        self.memory.iter_mut().for_each(|x| *x = 0);
+
+        // Reset timers
+        self.delay_timer = 0;
+        self.sound_timer = 0;
+
+        self.fontset_into_mem();
     }
 }
 
